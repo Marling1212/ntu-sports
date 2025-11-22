b35b9b0cc4b102183acefabb6e28a85faea33ec1
mastermarling <mastermarling@gmail.com>
2025-11-22 20:07:29 +0800

    修復 SeasonPlayDisplay：使用 getCourtDisplay 統一場地顯示

--- components/SeasonPlayDisplay.tsx
+++ components/SeasonPlayDisplay.tsx
@@ -4,6 +4,7 @@ import { Match, Player } from "@/types/tournament";
 import { useState, useMemo } from "react";
 import TournamentBracket from "./TournamentBracket";
 import { getCourtDisplay } from "@/lib/utils/getCourtDisplay";
+import { getCourtDisplay } from "@/lib/utils/getCourtDisplay";
 
 interface SeasonPlayDisplayProps {
   matches: Match[];
